37d926b6a47fa4585dd87ce8abfe94b5e3fba537
canto9112 <cantoweb112@gmail.com>
2021-02-18 09:53:35 +0300

    Переименовал функцию и переменные внутри нее

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def get_wines(file):
-    excel_data_df = pandas.read_excel(file, sheet_name='Лист1')
-    excel_wines = excel_data_df.to_dict(orient='record')
-    wines = collections.defaultdict(list)
-    for wine in excel_wines:
-        category = wine['Категория']
-        wines[category].append(wine)
-    return wines
+def get_products(file, sheet_name):
+    excel_data_df = pandas.read_excel(file, sheet_name=sheet_name)
+    excel_products = excel_data_df.to_dict(orient='record')
+    products = collections.defaultdict(list)
+    for product in excel_products:
+        category = product['Категория']
+        products[category].append(product)
+    return products
 
 
 def get_work_years(year):
@@ -22,13 +22,12 @@ def get_work_years(year):
     return work_years
 
 
-def get_index_html(year, wines):
+def rendered_index_html(year, wines):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-
     rendered_page = template.render(
         years=year,
         wines=wines
@@ -39,11 +38,13 @@ def get_index_html(year, wines):
 
 if __name__ == '__main__':
     foundation_year = 1920
+    products_file = 'wine3.xlsx'
+    sheet_name = 'Лист1'
 
     years_work = get_work_years(foundation_year)
-    wines = get_wines('wine3.xlsx')
+    wines = get_products(products_file, sheet_name)
 
-    get_index_html(years_work, wines)
+    rendered_index_html(years_work, wines)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
