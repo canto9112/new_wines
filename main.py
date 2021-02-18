c082614eaa3cc38f20ca9fdd38cc3295b79abb7e
canto9112 <cantoweb112@gmail.com>
2021-02-18 10:28:55 +0300

    Добавил аргументов в функцию

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ def get_experience_of_work_years(year):
     return current_year.year - year
 
 
-def fetch_index_html(experience_years, all_products, template_name):
+def fetch_index_html(experience_years, all_products, template_name, homepage, path):
     env = Environment(
-        loader=FileSystemLoader('.'),
+        loader=FileSystemLoader(path),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template(template_name)
@@ -31,7 +31,7 @@ def fetch_index_html(experience_years, all_products, template_name):
         years=experience_years,
         all_products=all_products
     )
-    with open('index.html', 'w', encoding="utf8") as file:
+    with open(homepage, 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
@@ -40,11 +40,13 @@ if __name__ == '__main__':
     products_file = 'wine3.xlsx'
     sheet_name = 'Лист1'
     template_name = 'template.html'
+    homepage_name = 'index.html'
+    file_path = '.'
 
     experience_years = get_experience_of_work_years(foundation_year)
     products = get_products(products_file, sheet_name)
 
-    fetch_index_html(experience_years, products, template_name)
+    fetch_index_html(experience_years, products, template_name, homepage_name)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
